3f848b9f14980a71b847ca7c04a88a743f38983b
peterpan2005codes <smpt.sampeter2005@gmail.com>
2026-04-18 23:04:56 +0530

    files updated on 18-04-2026 itself

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2795964614" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3060738213" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
